d9f35dc136295b305338113b44408143a03ce92f
minbyoungdae <80196999+minbyoungdae@users.noreply.github.com>
2022-01-06 03:45:36 +0900

    Feat: 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import logo from "./images/swf_logo.png";
+
+import ImageUploader from "./components/ImageUploader";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <img src={logo}></img>
+      <h1>닮은 꼴 찾기</h1>
+      <ImageUploader />
+    </Wrapper>
   );
 }
 

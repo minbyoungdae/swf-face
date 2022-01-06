ddf7ca6f02fc545f41f8e17a32a3d9b96bf55b12
minbyoungdae <80196999+minbyoungdae@users.noreply.github.com>
2022-01-07 03:15:29 +0900

    Fix: 카카오 공유하기 컨포넌트 삽입

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
+import React from "react";
 import styled from "styled-components";
 import logo from "./images/swf_logo.png";
+import "./index.css";
 
 import ImageUploader from "./components/ImageUploader";
+import ShareKakao from "./components/ShareKakao";
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
+  width: 70%;
+  min-height: 100vh;
+  margin: 0 auto;
+  background-color: #f6f7fa;
+`;
+
+const Title = styled.span`
+  color: #212f3c;
+  font-size: 4rem;
+  font-family: "Black Han Sans", sans-serif;
 `;
 
 function App() {
   return (
     <Wrapper>
       <img src={logo}></img>
-      <h1>닮은 꼴 찾기</h1>
+      <Title>스우파 닮은 꼴 찾기</Title>
       <ImageUploader />
+      <ShareKakao />
     </Wrapper>
   );
 }

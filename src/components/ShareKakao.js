bb217d0b6d47b603a9ba8ca898988867342f8c10
minbyoungdae <80196999+minbyoungdae@users.noreply.github.com>
2022-01-07 03:12:47 +0900

    Fix: 카카오 공유하기 기능 구현

---
+++ src/components/ShareKakao.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { KakaoLinkDefault } from "react-kakao-link";
+import styled from "styled-components";
+import kakaoLogo from "../images/kakaoLogo.jpeg";
+
+const ShareImage = styled.img`
+  width: 3rem;
+  border-radius: 50%;
+  margin-top: 2rem;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const ShareKakao = () => {
+  const template = {
+    objectType: "feed",
+    content: {
+      title: "스우파 닮은 꼴 찾기",
+      description: "스트릿 우먼 파이터 닮은 꼴 찾기 테스트",
+      imageUrl:
+        "https://media.istockphoto.com/vectors/letter-logo-formed-by-two-parallel-lines-with-noise-texture-vector-id1201523312?k=20&m=1201523312&s=612x612&w=0&h=muC5BerISf25lI9aWRDTUpWABqOADIO_OPfEX2ZkMU0=",
+      link: {
+        mobileWebUrl: "http://localhost:3000/",
+        webUrl: "http://localhost:3000/",
+      },
+    },
+    buttons: [
+      {
+        title: "웹으로 보기",
+        link: {
+          mobileWebUrl: "http://localhost:3000/",
+          webUrl: "http://localhost:3000/",
+        },
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <KakaoLinkDefault
+        template={template}
+        jsKey={"e346b1d37e833952b5657ea89d4760c4"}
+      >
+        <ShareImage src={kakaoLogo}></ShareImage>
+      </KakaoLinkDefault>
+    </div>
+  );
+};
+
+export default ShareKakao;
